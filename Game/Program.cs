5bfe310c65d4f15824b5f03a8c35099eaee66a3e
twennb <towe2400@student.miun.se>
2026-05-18 06:57:11 +0200

    Add project files.

---
+++ Game/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program()
+{
+    public static void Main(string[] args)
+    {
+        string userInput;
+
+        do
+        {
+            Console.WriteLine("> ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("You wrote '" + userInput + "'.");
+        } while (userInput != "q");
+    }
+}
