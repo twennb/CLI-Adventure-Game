68a284e384a4e2ac5e29acf68caf459ff68a2c6d
twennb <towe2400@student.miun.se>
2026-05-18 07:04:44 +0200

    Added basic structure for the room class which will handle each game location. String Name and Description will help the player understand where they are while the four ints N,S,E,W will store which room if any is in that direction.

---
+++ Game/Models/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace Game.Models;
+
+internal class Room
+{
+    private string Name { get; set; }
+    private string Description { get; set; }
+    private int N { get; set; }
+    private int S { get; set; }
+    private int E { get; set; }
+    private int W { get; set; }
+
+    public Room (string name, string desc, int n, int s, int e, int w)
+    {
+        Name = name;
+        Description = desc;
+        N = n;
+        S = s;
+        E = e;
+        W = w;
+    }
+}
